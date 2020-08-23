2690532291a919623a79a527da3d24c1bede928f
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2020-08-23 12:31:50 +0200

    Updated repeated table with default value of 0

--- server/db.js
+++ server/db.js
@@ -6,6 +6,23 @@ function connectDatabase() {
     if (!db) {
         db = mysql.createConnection(settings.dbConnecton);
 
+	//#### Optional code creating db table
+        //#### not recomended in real-time applications
+        //#### used for convinience
+        let createTableUrls = `create table if not exists urls(
+            id int primary key auto_increment,
+            urlCode varchar(255)not null,
+            longUrl varchar(255)not null,
+            shortUrl varchar(255)not null,
+            repeated int null default '0'
+        )`
+        
+        db.query(createTableUrls, (err,result,fields) => {
+            if(err) {
+                console.log(err.message);
+            } 
+        });
+
         db.connect(function(err){
             if(!err) {
                 console.log('Database is connected!');
